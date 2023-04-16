330715f6ed701e8f329ccc2d9d5ba3d768df40c2
Miguel Ángel García Moreno <miguelangel.gm262@gmail.com>
2023-04-16 21:28:15 +0200

    Added duplicate deletion from dataset using pandas

--- src/model.rs
+++ src/model.rs
@@ -41,8 +41,6 @@ impl Model {
         let predicted_value = self.coefficients[0] * floor_distance + self.coefficients[1] * velocity
         + self.coefficients[2] * obstacle_distance + self.coefficients[3] * gap_distance + self.intercept;
 
-        println!("{}", 1.0/(1.0 + (-predicted_value).exp()));
-
         if self.activation_func.eq(&String::from("step")) {
             if predicted_value >= 0.0 {
                 1
@@ -52,7 +50,7 @@ impl Model {
             }
         }
         else {
-            if 1.0/(1.0 + (-predicted_value).exp()) >= 0.5 {
+            if 1.0/(1.0 + (-predicted_value).exp()) >= 0.51 {
                 1
             }
             else {
